b3b3d970e887d2b6739fa6176e0b22f76f4f5556
瓜田 駿亮 <s_urita@waku-2.com>
2017-12-26 15:25:35 +0900

    add atom packages

---
+++ .atom/packages/atom-typescript/lib/typings/atom.d.ts
@@ -0,0 +1,12 @@
+export {}
+
+declare module "atom" {
+  export class CompositeDisposable {
+    add(disposable: AtomCore.Disposable)
+    dispose()
+  }
+
+  var TextBuffer: {
+    new (opts?: {filePath?: string; load?: boolean}): TextBuffer.ITextBuffer
+  }
+}
